64ab8036d9a3153895488ec8c1ea9093b9a643db
dobronskib <dobronskib@P21903>
2023-03-03 10:05:47 +0100

    add new tests

--- ClassLibrary1/StringCalculator.cs
+++ ClassLibrary1/StringCalculator.cs
@@ -4,7 +4,20 @@
     {
         public int Add(string numbers)
         {
-            return 0;
+            if(numbers == "") 
+                return 0;
+            if(numbers.Length > 1)
+            {
+                int sum = 0;
+                string[] splitNumbers = numbers.Split(',', '\n');
+                foreach (var splitNumber in splitNumbers)
+                {
+                    sum += int.Parse(splitNumber);
+                }
+                return sum;
+            }
+                
+            return int.Parse(numbers);
         }
     }
 }
--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -18,5 +18,67 @@ namespace TestProject1
             // Assert
             Assert.Equal(expected, result);
         }
+
+        [Theory]
+        [InlineData("1", 1)]
+        [InlineData("2", 2)]
+        [InlineData("3", 3)]
+        [InlineData("4", 4)]
+        [InlineData("5", 5)]
+        [InlineData("6", 6)]
+        public void ItShouldReturnValue_WhenSingleNumberProvided(string input, int expected)
+        {
+            // Arrange
+            StringCalculator stringCalculator = new();
+            int result;
+
+            // Act
+            result = stringCalculator.Add(input);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("1,3", 4)]
+        [InlineData("2,5", 7)]
+        [InlineData("3,6", 9)]
+        [InlineData("4,2", 6)]
+        [InlineData("5,1", 6)]
+        [InlineData("6,3", 9)]
+        [InlineData("-6,3", -3)]
+        public void ItShouldReturnSumOfValues_WhenTwoNumbersProvided(string input, int expected)
+        {
+            // Arrange
+            StringCalculator stringCalculator = new();
+            int result;
+
+            // Act
+            result = stringCalculator.Add(input);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("1\n3", 4)]
+        [InlineData("2\n5", 7)]
+        [InlineData("3\n6", 9)]
+        [InlineData("4\n2", 6)]
+        [InlineData("5\n1", 6)]
+        [InlineData("6\n3", 9)]
+        [InlineData("-6\n3", -3)]
+        public void ItShouldReturnSumOfValues_WhenTwoNumbersProvided_WhenSplitIsNevline (string input, int expected)
+        {
+            // Arrange
+            StringCalculator stringCalculator = new();
+            int result;
+
+            // Act
+            result = stringCalculator.Add(input);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
     }
 }
